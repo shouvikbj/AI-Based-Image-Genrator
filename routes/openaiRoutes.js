da418c5a27610d77551e2985fce00cd9f7d84ca1
shouvikbj <bajpayeeshouvik@gmail.com>
2022-12-13 13:56:23 +0530

    first commit

---
+++ routes/openaiRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const {generateImage} = require("../controllers/openaiController");
+
+router.post('/generateimage', generateImage)
+
+module.exports = router
